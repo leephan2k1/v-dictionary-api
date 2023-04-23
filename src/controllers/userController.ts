65cd138f471b232c61377c57198b6f59e978e5e2
leephan2k1 <pttl2k1@gmail.com>
2023-04-24 01:38:54 +0700

    feat: 🎸 CRUD translation history

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import type { NextFunction, Request, Response } from "express";
+import { prisma } from "../utils/prismaClient";
 
 export async function handleGetUserInfo(
   req: Request,
@@ -10,5 +11,119 @@ export async function handleGetUserInfo(
   } catch (error) {
     console.error("handleGetUserInfo ERROR: ", error);
     return res.status(500).json("ERROR");
+  }
+}
+
+interface TranslationHistoryBody {
+  word: string;
+  sense: string;
+  currentLanguage: string;
+  targetLanguage: string;
+}
+
+export async function handleCreateTranslationHistory(
+  req: Request<{}, {}, TranslationHistoryBody, {}>,
+  res: Response,
+  next: NextFunction
+) {
+  const { word, sense, currentLanguage, targetLanguage } = req.body;
+  const { user } = req;
+
+  try {
+    const tHistory = await prisma.translationHistory.upsert({
+      where: {
+        userId_word: {
+          //@ts-ignore
+          userId: user.id,
+          word,
+        },
+      },
+      create: {
+        word,
+        sense,
+        currentLanguage,
+        targetLanguage,
+        //@ts-ignore
+        userId: user.id,
+      },
+      update: {},
+    });
+
+    return res.status(201).json(tHistory);
+  } catch (error) {
+    console.error(`handleCreateTranslationHistory: ${error}`);
+    next("handleCreateTranslationHistory ERROR");
+  }
+}
+
+export async function handleGetTranslationHistory(
+  req: Request<{}, {}, {}, { page: number; limit: number }>,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { user } = req;
+    let { page, limit } = req.query;
+
+    if (!page) page = 1;
+    if (!limit) limit = 18;
+
+    const [translations, totalRecords] = await prisma.$transaction([
+      prisma.translationHistory.findMany({
+        //@ts-ignore
+        where: { userId: user.id },
+        orderBy: {
+          createdAt: "desc",
+        },
+        take: Number(limit),
+        skip: (Number(page) - 1) * limit,
+      }),
+      prisma.translationHistory.count({
+        //@ts-ignore
+        where: { userId: user.id },
+      }),
+    ]);
+
+    return res
+      .status(200)
+      .json({ translations, totalPages: Math.ceil(totalRecords / limit) });
+  } catch (error) {
+    console.error(`handleGetTranslationHistory: ${error}`);
+    next("handleGetTranslationHistory ERROR");
+  }
+}
+
+export async function handleDeleteTranslationHistory(
+  req: Request<{}, {}, { word: string; deleteOption: string }, {}>,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    const { word, deleteOption } = req.body;
+    const { user } = req;
+
+    if (deleteOption === "deleteAll") {
+      const deleteStatus = await prisma.translationHistory.deleteMany({
+        //@ts-ignore
+        where: { userId: user.id },
+      });
+
+      return res.status(200).json({ deleteStatus });
+    }
+
+    const deleteStatus = await prisma.translationHistory.delete({
+      where: {
+        userId_word: {
+          word,
+          //@ts-ignore
+          userId: user.id,
+        },
+      },
+    });
+
+    return res.status(200).json({ deleteStatus });
+  } catch (error) {
+    console.error(`handleDeleteTranslationHistory: ${error}`);
+    next("handleDeleteTranslationHistory ERROR");
   }
 }
--- src/router/user.ts
+++ src/router/user.ts
@@ -1,9 +1,41 @@
 import Router from "express-promise-router";
 import { isUserAuthenticated } from "../middlewares/auth";
-import { handleGetUserInfo } from "../controllers/userController";
+import {
+  handleGetUserInfo,
+  handleCreateTranslationHistory,
+  handleGetTranslationHistory,
+  handleDeleteTranslationHistory,
+} from "../controllers/userController";
+import {
+  TranslationHistorySchema,
+  DeleteTranslationHistorySchema,
+} from "../schemas";
+import { validate } from "../middlewares/zodValidate";
 
 const router = Router();
 
 router.get("/auth/user", isUserAuthenticated, handleGetUserInfo);
+
+router.post(
+  "/users/translation-history",
+  isUserAuthenticated,
+  validate(TranslationHistorySchema),
+  handleCreateTranslationHistory
+);
+
+router.get(
+  "/users/translation-history",
+  isUserAuthenticated,
+  //@ts-ignore
+  handleGetTranslationHistory
+);
+
+router.delete(
+  "/users/translation-history",
+  isUserAuthenticated,
+  validate(DeleteTranslationHistorySchema),
+  //@ts-ignore
+  handleDeleteTranslationHistory
+);
 
 export default router;
